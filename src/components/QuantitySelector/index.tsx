808c6392aa489fe7fcec0e134c4defa2eb40a8e3
Abraham E. Tavarez <abetavarez@icloud.com>
2022-10-22 22:07:59 -0400

    quantity selector state

--- src/components/QuantitySelector/index.tsx
+++ src/components/QuantitySelector/index.tsx
@@ -1,22 +1,50 @@
-import {View, Text, Pressable} from 'react-native';
 import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
-const QuantitySelector = () => {
-  const onMinus = () => {};
-  const onPlus = () => {};
+const QuantitySelector = ({quantity, setQuantity}) => {
+  const onMinus = () => {
+    setQuantity(Math.max(0, quantity - 1));
+  };
+  const onPlus = () => {
+    setQuantity(quantity + 1);
+  };
   return (
-    <View>
-      <Pressable onPress={onMinus}>
-        <Text>-</Text>
+    <View style={styles.root}>
+      <Pressable onPress={onMinus} style={styles.button}>
+        <Text style={styles.buttonText}>-</Text>
       </Pressable>
 
-      <Text>0</Text>
+      <Text style={styles.quantity}>{quantity}</Text>
 
-      <Pressable onPress={onPlus}>
-        <Text>+</Text>
+      <Pressable onPress={onPlus} style={styles.button}>
+        <Text style={styles.buttonText}>+</Text>
       </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 130,
+    borderWidth: 1,
+    borderColor: '#e3e3e3',
+  },
+  button: {
+    width: 35,
+    height: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d1d1d1',
+  },
+  buttonText: {
+    fontSize: 18,
+  },
+  quantity: {
+    color: '#007eb9',
+  },
+});
 
 export default QuantitySelector;
