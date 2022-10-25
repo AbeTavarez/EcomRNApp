647326fd26226ffa3dba29d9a070d7db35722a62
Abraham E. Tavarez <abetavarez@icloud.com>
2022-10-24 22:58:18 -0400

    shopping cart screen created

---
+++ src/screens/ShoppingCartScreen/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import CartProductItem from '../../components/CartProductItem';
+import products from '../../data/cart';
+
+const ShoppingCartScreen = () => {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={products}
+        renderItem={({item}) => <CartProductItem cartItem={item} />}
+        keyExtractor={({id}) => id}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  },
+});
+
+export default ShoppingCartScreen;
